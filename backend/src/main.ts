5a57d60a65ab99f240f039fb3a05d361b78a628c
santiagoTrivi <santiagocarvajal103@gmail.com>
2023-08-24 15:13:39 -0400

    backend: cors, vercel file configuration

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { CORS } from './config/cors-constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,6 +18,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('doc', app, document);
 
+  app.enableCors(CORS)
   await app.listen(3000);
 }
 bootstrap();
