63e9bfbd542c58cbbb4c9d28fbe5350db26f4143
santiagoTrivi <santiagocarvajal103@gmail.com>
2023-08-24 15:57:53 -0400

    refactoring JWT-CONFIG path in the auth module

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -3,16 +3,14 @@ import { CardService } from './infrastructure/services/card.service';
 import { AuthController } from './auth.controller';
 import { ClientService } from './infrastructure/services/client.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { JWT_CONFIG } from 'src/config/jwt.cofig';
 import { Client, ClientSchema } from './infrastructure/schemas/client.schema';
 import { Card, CardSchema } from './infrastructure/schemas/card.schema';
 import { LocalStrategy } from './infrastructure/strategies/local.strategy';
 import { AuthService } from './infrastructure/services/auth.service';
-import { JwtModule, JwtService } from '@nestjs/jwt';
-import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './infrastructure/strategies/jwt.Strategy';
 import { GetCardInfo } from './useCase/getCardInfo';
 import { RefreshTokenStrategy } from './infrastructure/strategies/refresh.jwt.strategy';
+import { JWT_CONFIG } from '../config/jwt.cofig';
 
 @Module({
   imports: [
--- backend/src/auth/infrastructure/strategies/local.strategy.ts
+++ backend/src/auth/infrastructure/strategies/local.strategy.ts
@@ -2,7 +2,6 @@ import { Strategy } from 'passport-local';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from '../services/auth.service';
-import { LoginDto } from '../dto/login-dto';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy, 'local') {
